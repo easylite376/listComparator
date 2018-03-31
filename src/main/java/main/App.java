d70ea70d498e5f3d74b727ab029a39f04828dd15
Martin Stähr <martin.staehr@flutaro.de>
2018-03-31 23:22:02 +0200

    removing unused test, adding first example for reading xlsx files, moving packages

---
+++ src/main/java/main/App.java
@@ -0,0 +1,73 @@
+package main;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Iterator;
+
+public class App {
+
+    public static void main(String[] args) {
+        try {
+            // TODO: reading date as date
+            String excelFilePath = "listPlus.xlsx";
+            FileInputStream inputStream = new FileInputStream(new File(excelFilePath));
+            System.out.println(excelFilePath);
+            Workbook workbook = getRelevantWorkbook(inputStream, excelFilePath);
+
+            Sheet firstSheet = workbook.getSheetAt(0);
+            Iterator<Row> iterator = firstSheet.iterator();
+
+            while (iterator.hasNext()) {
+                Row nextRow = iterator.next();
+                Iterator<Cell> cellIterator = nextRow.cellIterator();
+                while (cellIterator.hasNext()) {
+                    Cell cell = cellIterator.next();
+                    switch (cell.getCellTypeEnum()) {
+                        case STRING:
+                            System.out.print(cell.getStringCellValue());
+                            break;
+                        case NUMERIC:
+                            System.out.print(cell.getNumericCellValue());
+                            break;
+                        case BOOLEAN:
+                            System.out.print(cell.getBooleanCellValue());
+                            break;
+                        default:
+                            break;
+                    }
+                    System.out.print(" ");
+                }
+                System.out.println();
+            }
+
+            workbook.close();
+            inputStream.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static Workbook getRelevantWorkbook(FileInputStream inputStream, String excelFilePath) throws IOException {
+        Workbook workbook;
+
+        if (excelFilePath.endsWith("xls")) {
+            workbook = new HSSFWorkbook(inputStream);
+        } else if (excelFilePath.endsWith("xlsx")) {
+            workbook = new XSSFWorkbook(inputStream);
+        } else {
+            throw new IllegalArgumentException("Incorrect file format");
+        }
+
+        return workbook;
+    }
+
+}
